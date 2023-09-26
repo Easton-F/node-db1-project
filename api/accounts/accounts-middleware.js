69b047c677712a08cd89ba0f7a2cdacae018f734
Easton <easton.fritsch21@gmail.com>
2023-09-26 13:36:24 -0600

    WIP

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,28 +1,46 @@
 const Account = require('./accounts-model')
+const db = require('../../data/db-config')
 
 exports.checkAccountPayload = (req, res, next) => {
-  const error = { status: 400 }
   const { name, budget } = req.body
   if (name === undefined || budget === undefined) {
-    error.message = 'name and budget are required'
-    next(error)
+    res.status(400).json({
+      message: 'name and budget are required',
+    })
   } else if (typeof name !== 'string') {
-    error.message = 'name of account must be a string'
-    next(error)
+    res.status(400).json({
+      message: 'name of account must be a string',
+    })
   } else if (name.trim().length < 3 || name.trim().length > 100) {
-    error.message = 'name of account must be between 3 and 100'
-    next(error)
+    res.status(400).json({
+      message: 'name of account must be between 3 and 100',
+    })
   } else if (budget < 0 || budget > 1000000) {
-    error.message = 'budget of account is too large or too small'
-    next(error)
+    res.status(400).json({
+      message: 'budget of account is too large or too small',
+    })
   } else if (typeof budget !== 'number' || isNaN(budget)) {
-    error.message = 'budget of account must be a number'
-    next(error)
+    res.status(400).json({
+      message: 'budget of account must be a number',
+    })
+  } else {
+    next()
   }
 }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try {
+    const existingName = await db('accounts').where('name', req.body.name.trim()).first()
+    if (existingName) {
+      res.status(400).json({
+        message: 'that name is taken',
+      })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 exports.checkAccountId = async (req, res, next) => {
